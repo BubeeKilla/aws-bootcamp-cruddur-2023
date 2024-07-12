510b7700f9e8781d98d4ea2b3d7d544542f91e7c
Eugen Gaibu <eugengaibu@gmail.com>
2024-07-12 12:50:37 +0000

    update

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,17 +1,11 @@
 from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
 class NotificationsActivities:
   def run():
-    #xray--------
-    segment = xray_recorder.begin_segment('notification_activities')
-    #------------
-
     now = datetime.now(timezone.utc).astimezone()
-
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'bubeekilla',
-      'message': 'i kill boobs',
+      'handle':  'coco',
+      'message': 'I am white unicorn',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -20,20 +14,13 @@ class NotificationsActivities:
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
-        'message': 'This post has no honor!',
+        'handle':  'worf',
+        'message': 'this post has no honor!',
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
         'created_at': (now - timedelta(days=2)).isoformat()
       }],
-    }]
-
-    #xray
-    dict = {
-      "now": now.isoformat(),
-      "result": len(model['data'])
     }
-
-    subsegment = xray_recorder.begin_subsegment('mock-data')
+    ]
     return results
